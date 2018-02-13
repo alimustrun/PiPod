eeaeaadd102689c9f52ffb7ad3a0bba7fac8ad04
Harold Fritsch <hfritsch_ext@moncoyote.com>
2018-02-13 15:13:37 +0100

    Added GoogleTest

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+#include <gtest/gtest.h>
 #include "screen-driver/ScreenDriver.h"
+#include "Number.h"
 
-int main() {
-    ScreenDriver *screenDriver = new ScreenDriver();
-    screenDriver->displaySomething();
+int main(int argc, char **argv) {
+//    ScreenDriver *screenDriver = new ScreenDriver();
+//    screenDriver->displaySomething();
+   // Number *number = new Number();
+   // std::cout << number->isOdd(1) << std::flush;
+    ::testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
     return 0;
 }
