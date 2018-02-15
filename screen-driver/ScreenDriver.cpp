000083fedb35376a7ec3c23063c02abc46ef7e5f
Harold Fritsch <hfritsch_ext@moncoyote.com>
2018-02-15 13:53:28 +0100

    Clearing buffer at screen driver startup

--- screen-driver/ScreenDriver.cpp
+++ screen-driver/ScreenDriver.cpp
@@ -10,12 +10,14 @@ ScreenDriver::ScreenDriver()
         printf("e-Paper init failed\n");
         return;
     }
+    fullClear();
     _epd->DelayMs(500);
     if (_epd->Init(lut_partial_update) != 0)
     {
         printf("e-Paper init failed\n");
         return;
     }
+    fullClear();
     _frame_buffer = (unsigned char*)malloc(_epd->width / 8 * _epd->height);
     _paint = new Paint(_frame_buffer, _epd->width, _epd->height);
     _paint->SetRotate(ROTATE_90);
