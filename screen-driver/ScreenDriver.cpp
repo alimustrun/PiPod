17363e42df89d2f8362869dd39e7dca4e97e7128
Harold Fritsch <hfritsch_ext@moncoyote.com>
2018-02-16 17:43:23 +0100

    Adding a cursor functionnality

--- screen-driver/ScreenDriver.cpp
+++ screen-driver/ScreenDriver.cpp
@@ -142,7 +142,9 @@ const void ScreenDriver::displayCursor(int currentSelection)
     _paint->SetWidth(128);
     _paint->SetHeight(24);
     _paint->Clear(COLORED);
-    _paint->DrawStringAt(12 + ((currentSelection - 1) * 24), 0, ">", &Font24, UNCOLORED);
+    _paint->DrawStringAt(0, 116 - ((currentSelection - 1) * 24), ">", &Font24, UNCOLORED);
+    _epd->SetFrameMemory(_paint->GetImage(), 0, 0, _paint->GetWidth(), _paint->GetHeight());
+    _epd->DisplayFrame();
     _epd->SetFrameMemory(_paint->GetImage(), 0, 0, _paint->GetWidth(), _paint->GetHeight());
     _epd->DisplayFrame();
     _paint->SetWidth(_epd->width);
