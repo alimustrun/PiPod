b2edd50e5264abbbdf95ba4b84fc878c21a3f1df
Harold Fritsch <hfritsch_ext@moncoyote.com>
2018-02-21 15:54:42 +0100

    asciify 3rd attempt

--- Utils.cpp
+++ Utils.cpp
@@ -4,6 +4,7 @@
 
 #include <dirent.h>
 #include <cstddef>
+#include <cstring>
 #include "Utils.h"
 
 void Utils::getFilesFromPath(std::vector<const char *> *filenames, const char *path)
@@ -11,7 +12,10 @@ void Utils::getFilesFromPath(std::vector<const char *> *filenames, const char *p
     DIR* dirp = opendir(path);
     struct dirent * dp;
     while ((dp = readdir(dirp)) != NULL) {
-        filenames->push_back(dp->d_name);
+        if (strlen(dp->d_name) > 1 && (dp->d_name[0] != '.' || (dp->d_name[0] == '.' && dp->d_name[1] == '.')))
+        {
+            filenames->push_back(dp->d_name);
+        }
     }
     closedir(dirp);
 }
