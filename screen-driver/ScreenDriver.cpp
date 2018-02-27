8307526105e69075740e3d134ec31e7b84f4b0d3
Harold Fritsch <hfritsch_ext@moncoyote.com>
2018-02-27 11:22:10 +0100

    Improved clearscreen

--- screen-driver/ScreenDriver.cpp
+++ screen-driver/ScreenDriver.cpp
@@ -19,12 +19,16 @@ ScreenDriver::ScreenDriver()
 const void ScreenDriver::fullClear()
 {
     _frame_buffer = (unsigned char*)malloc(_epd->width / 8 * _epd->height);
+    memset(_frame_buffer, UNCOLORED, _epd->width / 8 * _epd->height);
+    _epd->SetFrameMemory(_frame_buffer, 0, 0, _epd->width, _epd->height);
+    _epd->DisplayFrame();
+    _epd->SetFrameMemory(_frame_buffer, 0, 0, _epd->width, _epd->height);
+    _epd->DisplayFrame();
     memset(_frame_buffer, COLORED, _epd->width / 8 * _epd->height);
     _epd->SetFrameMemory(_frame_buffer, 0, 0, _epd->width, _epd->height);
     _epd->DisplayFrame();
     _epd->SetFrameMemory(_frame_buffer, 0, 0, _epd->width, _epd->height);
     _epd->DisplayFrame();
-
 }
 
 const void ScreenDriver::displayBootScreen()
