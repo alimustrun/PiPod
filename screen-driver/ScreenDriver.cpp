2859743c35fd1615581a6646e1eee44e605d0dbf
Harold Fritsch <hfritsch_ext@moncoyote.com>
2018-02-27 11:35:20 +0100

    Improved clearscreen

--- screen-driver/ScreenDriver.cpp
+++ screen-driver/ScreenDriver.cpp
@@ -18,14 +18,15 @@ ScreenDriver::ScreenDriver()
 
 const void ScreenDriver::fullClear()
 {
-//    _paint->Clear(UNCOLORED);
-//    _epd->SetFrameMemory(_frame_buffer, 0, 0, _paint->GetWidth(), _paint->GetHeight());
-//    _epd->DisplayFrame();
-//    _paint->Clear(COLORED);
-//    _epd->SetFrameMemory(_frame_buffer, 0, 0, _paint->GetWidth(), _paint->GetHeight());
-//    _epd->DisplayFrame();
-//    _epd->SetFrameMemory(_frame_buffer, 0, 0, _paint->GetWidth(), _paint->GetHeight());
-//    _epd->DisplayFrame();
+    _frame_buffer = (unsigned char*)malloc(_epd->width / 8 * _epd->height);
+    _paint->Clear(UNCOLORED);
+    _epd->SetFrameMemory(_frame_buffer, 0, 0, _paint->GetWidth(), _paint->GetHeight());
+    _epd->DisplayFrame();
+    _paint->Clear(COLORED);
+    _epd->SetFrameMemory(_frame_buffer, 0, 0, _paint->GetWidth(), _paint->GetHeight());
+    _epd->DisplayFrame();
+    _epd->SetFrameMemory(_frame_buffer, 0, 0, _paint->GetWidth(), _paint->GetHeight());
+    _epd->DisplayFrame();
 }
 
 const void ScreenDriver::displayBootScreen()
